3788003dc10c370bc5726c8493ab40fb2dde7ae0
Cts_max_ <n23342624@gmail.com>
2025-05-23 10:59:36 -0600

    Update cpmcheats.py

--- cpmcheats.py
+++ cpmcheats.py
@@ -4,7 +4,7 @@ import requests
 import subprocess
 import urllib.parse
 
-__ENDPOINT_URL__: str = "https://cpmcheats.abccloud.com.br/shadowAPi"
+__ENDPOINT_URL__: str = "https://cpmcheats.hostzera.com.br/shadowAPi"
 
 class CPMCheats:
     def __init__(self, access_key) -> None:
